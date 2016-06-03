2dba1db4d39f9e270ea715135dc00d54bc1b706b
schultzberg <viktor.schultzberg@gmail.com>
2016-06-03 14:10:39 +0200

    First try carousel!

--- functions.php
+++ functions.php
@@ -7,3 +7,5 @@
  */
 
 include 'lib/custom_post_types.php';
+
+wp_enqueue_style('theme-name-style', get_stylesheet_directory_uri() );
--- index.php
+++ index.php
@@ -21,20 +21,65 @@
 
 <!-- Bootstrap v4 Alpha -->
 <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css" integrity="sha384-XXXXXXXX" crossorigin="anonymous">
-<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js" integrity="sha384-XXXXXXXX" crossorigin="anonymous"></script>
-
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
 	<div id="page" class="site">
 		<header id="masthead" class="site-header" role="banner">
-			
+			<div class="fix-to-top"> <!-- fix header to top -->
+				<div class="container"> <!-- margin right/left auto, padding left/right 15 -->
+					<button></button> <!-- logo -->
+					<nav> <!-- menu med ul -->
+						
+					</nav>
+				</div>
+			</div>
 		</header><!-- #masthead -->
 
-		<div id="content" class="site-content">
-
+		<div id="content" class="site-content" role="document">
+			<div id="frontpage-carousel" class="carousel slide" data-ride="carousel"> <!-- fullscreen -->
+				<ol class="carousel-indicators">
+					<li data-target="#frontpage-carousel" data-slide-to="0 active"></li>
+					<li data-target="#frontpage-carousel" data-slide-to="1"></li>
+					<li data-target="#frontpage-carousel" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner" role="listbox">
+					<div class="carousel-item active">
+						<img class="first-slide" src="" alt="First slide">
+						<div class="container">
+							<div class="carousel-caption text-xs-left">
+								<p> 
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit enim quia cum cupiditate, earum qui voluptates ad vero, ab odit perferendis! Amet distinctio accusantium, minima assumenda. Similique necessitatibus quam maxime.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="second-slide" src="" alt="Second slide">
+						<div class="container">
+							<div class="carousel-caption text-xs-right">
+								<p> 
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit enim quia cum cupiditate, earum qui voluptates ad vero, ab odit perferendis! Amet distinctio accusantium, minima assumenda. Similique necessitatibus quam maxime.
+								</p>
+							</div>
+						</div>
+					</div>
+					<a class="left carousel-control" href="frontpage-carousel" role="button" data-slide="prev">
+						prev
+					</a>
+					<a class="right carousel-control" href="frontpage-carousel" role="button" data-slide="next">
+						next
+					</a>
+				</div>
+			</div>
+			<section> <!-- content -->
+				<div class="container">
+					<div class="row"></div>
+				</div>
+			</section>
 			
 
 		</div><!-- #content -->
@@ -45,6 +90,10 @@
 	</div><!-- #page -->
 
 	<?php wp_footer(); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js" integrity="sha384-XXXXXXXX" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
