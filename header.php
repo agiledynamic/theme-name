26b9657dc199e8d0ea4c348a8f5da4604609f1d4
schultzberg <viktor.schultzberg@gmail.com>
2016-06-09 10:05:57 +0200

    made header.php and footer.php

--- header.php
+++ header.php
@@ -8,5 +8,41 @@
  *
  * @package theme-name
  */
-
 ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<nav class="navbar navbar-fixed-top navbar-light bg-faded">
+		<div class="container">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler hidden-sm-up pull-xs-right" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+			&#9776;
+			</button>
+			<div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+				<ul class="nav navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Features</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Pricing</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">About</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
