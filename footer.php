ecb0d89b8d05070fe3906d255f3eec97a26d7155
schultzberg <viktor.schultzberg@gmail.com>
2016-06-23 11:07:35 +0200

    final changes in footer

--- footer.php
+++ footer.php
@@ -12,32 +12,33 @@
 ?>
 	<div class="<?php echo get_theme_mod('layout_container', 'container'); ?> p-y-1">
 		<div class="row contact">
-			<?php $query_about = new WP_Query( 'post_type=about&posts_per_page=1' ); ?>
-			<?php $query_contact = new WP_Query( 'post_type=contact&posts_per_page=1' ); ?>
+		<?php $query_about = new WP_Query( 'post_type=about&posts_per_page=1' ); ?>
+		<?php $query_contact = new WP_Query( 'post_type=contact&posts_per_page=1' ); ?>
 
-			<?php if( $query_about->have_posts() && $query_contact->have_posts() ) : ?>
-				<div class="hidden-sm-down text-xs-center text-md-left col-md-4 col-lg-4 p-y-1 about secondary-color nav-font">
-			<?php elseif( $query_about->have_posts() && $query_contact->have_posts() == false ) : ?>
-				<div class="hidden-sm-down text-md-left col-md-6 col-lg-6 p-y-1 about secondary-color nav-font">
-			<?php else :  ?>
-				<div>
-			<?php endif; ?>
+		<?php if( $query_about->have_posts() && $query_contact->have_posts() ) : ?>
+			<div class="hidden-sm-down text-xs-center text-md-left col-md-4 col-lg-4 p-y-1 about secondary-color nav-font">
+		<?php elseif( $query_about->have_posts() && $query_contact->have_posts() == false ) : ?>
+			<div class="hidden-sm-down text-md-left col-md-6 col-lg-6 p-y-1 about secondary-color nav-font">
+		<?php else :  ?>
+			<div>
+		<?php endif; ?>
 			
-			<?php if( $query_about->have_posts() ) : while( $query_about->have_posts() ) : $query_about->the_post(); ?>
-				<?php $title= str_ireplace('"', '', trim(get_the_title())); ?>
-				<?php $desc= str_ireplace('"', '', trim(get_the_content())); ?>
-					<p><strong><?= $title ?></strong></p>
-					<p><?= $desc; ?></p>
+		<?php if( $query_about->have_posts() ) : while( $query_about->have_posts() ) : $query_about->the_post(); ?>
+			<?php $title= str_ireplace('"', '', trim(get_the_title())); ?>
+			<?php $desc= str_ireplace('"', '', trim(get_the_content())); ?>
+				<p><strong><?= $title; ?></strong></p>
+				<p><?= $desc; ?></p>
 			<?php endwhile; endif; ?>
-				</div> <!-- .about -->
+			</div> <!-- .about -->
 
 			<?php if( $query_contact->have_posts() && $query_about->have_posts() ) : ?>
-				<div class="col-xs-12 text-xs-center text-md-left col-md-4 p-y-1 pull-md-right contact-adress secondary-color nav-font">
+				<div class="col-xs-12 text-xs-center text-md-right col-md-4 p-y-1 pull-md-right contact-adress secondary-color nav-font">
 			<?php elseif( $query_contact->have_posts() && $query_about->have_posts() == false) : ?>
 				<div class="col-xs-12 col-md-6 p-y-1 text-xs-center text-md-left contact-adress secondary-color nav-font">
 			<?php else: ?>
 				<div>
 			<?php endif; ?>
+
 			<?php if( $query_contact->have_posts() ) : while( $query_contact->have_posts() ) : $query_contact->the_post(); 
 				$title = str_ireplace('"', '', trim(get_the_title())); 
 				$homepage = str_ireplace ( array('www.', 'http://'), '', trim(get_post_meta($post->ID, 'webpage', true)));
@@ -46,52 +47,52 @@
 				$instagram = get_post_meta($post->ID, 'instagram', true); 
 				$twitter = get_post_meta($post->ID, 'twitter', true);
 				$adress = get_post_meta($post->ID, 'street', true) . " " . get_post_meta($post->ID, 'zipcode', true) . " " . get_post_meta($post->ID, 'city', true);
-			?>	
-				<p><strong><?= $title ?></strong></p>
-				<adress>
-					<span><a class="secondary-color" href="http://maps.google.com/?q=<?= $adress; ?>"> <?= $adress; ?> </a></span>
-					<span><a class="secondary-color" href="mailto:<?= get_post_meta($post->ID, 'email', true) ?>"><?= get_post_meta($post->ID, 'email', true) ?></a></span>
-					<span><?= get_post_meta($post->ID, 'phone', true) ?></span>
+			?>
+				<p><strong><?= $title; ?></strong></p>
+
+				<div class="adress">
+					<span><a class="secondary-color" href="http://maps.google.com/?q=<?= str_ireplace(' ', '+', trim($adress)); ?>"> <?= $adress; ?> </a></span>
+					<span><a class="secondary-color" href="mailto:<?= get_post_meta($post->ID, 'email', true); ?>"><?= get_post_meta($post->ID, 'email', true); ?></a></span>
+					<span><?= get_post_meta($post->ID, 'phone', true); ?></span>
 					<span><a class="secondary-color" href="http://www.<?= $homepage; ?>"> <?= $homepage; ?></a></span> 
-				</adress>
-				</div> <!-- .contact -->
-				<?php if ($query_about->have_posts() == false ) : ?>
-				<div class="col-xs-12 text-xs-center col-md-6 text-md-left p-y-1 contact-socialmedia">
-				<?php else: ?>
-				<div class="col-xs-12 text-xs-center col-md-4 text-md-left p-y-1 contact-socialmedia">
-				<?php endif; ?>
-					<ul>
-						<?php if ($facebook): ?>
-						<li>
-							<a class="secondary-color" href="http://www.facebook.com/<?= $facebook; ?>">
-								<i class="fa fa-facebook-official" aria-hidden="true"></i><?= $facebook; ?>
-							</a>
-						</li>
-						<?php endif; ?>
-						<?php if ($linkedin): ?>
-						<li>
-							<a class="secondary-color" href="http://www.linkedin.com/in/<?= $linkedin; ?> ">
-								<i class="fa fa-linkedin" aria-hidden="true"></i><?= $linkedin; ?>
-							</a>
-						</li>
-						<?php endif; ?>
-						<?php if($instagram): ?>
-						<li>
-							<a class="secondary-color" href="http://www.instagram.com/<?= $instagram; ?> ">
-								<i class="fa fa-instagram" aria-hidden="true"></i><?= $instagram; ?>
-								</a>
-						</li>
-						<?php endif; ?>
-						<?php if($twitter): ?>
-						<li>
-							<a class="secondary-color" href="http://www.twitter.com/<?= $twitter; ?>">
-								<i class="fa fa-twitter" aria-hidden="true"></i><?= $twitter; ?>
-							</a> 
-						</li>
-						<?php endif; ?>
-					</ul>
 				</div>
-				<?php endwhile; endif; ?>
+			</div> <!-- .contact -->
+			<?php if ($query_about->have_posts() == false ) : ?>
+			<div class="col-xs-12 text-xs-center col-md-6 text-md-left p-y-1 contact-socialmedia">
+			<?php else: ?>
+			<div class="col-xs-12 text-xs-center col-md-4 p-y-1 contact-socialmedia">
+			<?php endif; ?>
+				<ul>
+				<?php if ($facebook): ?>
+					<li>
+						<a class="secondary-color" href="http://www.facebook.com/<?= $facebook; ?>">
+							<i class="fa fa-facebook-official" aria-hidden="true"></i><?= $facebook; ?>
+						</a>
+					</li>
+				<?php endif; ?>
+				<?php if ($linkedin): ?>
+					<li>
+						<a class="secondary-color" href="http://www.linkedin.com/in/<?= $linkedin; ?>">
+							<i class="fa fa-linkedin" aria-hidden="true"></i><?= $linkedin; ?>
+						</a>
+					</li>
+				<?php endif; ?>
+				<?php if($instagram): ?>
+					<li>
+						<a class="secondary-color" href="http://www.instagram.com/<?= $instagram; ?>">
+							<i class="fa fa-instagram" aria-hidden="true"></i><?= $instagram; ?>
+						</a>
+					</li>
+				<?php endif; ?>
+				<?php if($twitter): ?>
+					<li>
+						<a class="secondary-color" href="http://www.twitter.com/<?= $twitter; ?>">
+							<i class="fa fa-twitter" aria-hidden="true"></i><?= $twitter; ?>
+						</a> 
+					</li>
+				<?php endif; ?>
+				</ul>
+			<?php endwhile; endif; ?>
 			</div>
 		</div> <!-- .row .contact -->
 		<div class="row">
